20aaab5fcc04d4e32d23cd47a10552fdac61e0b2
Carlo Lancia <clancia@MacBook-Pro-di-Carlo.local>
2015-02-17 12:09:52 +0100

    Decorator + comments
    Fullsigma is now accessible as an attribute of the class through a
    decorator
    Methods commented

--- parall_tasep.py
+++ parall_tasep.py
@@ -2,24 +2,28 @@
 
 import numpy as np
 import numpy.random as npr
-import time
-import datetime
-import pdb
-
-def densityReduction(v):
-    return sum(v)/float(len(v))
+#import time
+#import datetime
+#import pdb
 
 class Tasep(object):
-    def __init__(self, particles, p, epsilon):
+    def __init__(self, particles, size=None, p=.5, epsilon=.05):
         """
         Parallel TASEP on a 1D-ring of length 2*particles with update probability and blockage intensity epsilon
         """
         self.b = particles
-        self.n = 2*self.b
+        # how many particles in the tasep
+        if size is not None:
+            self.n = size
+        else:
+            self.n = 2*self.b
+            # the size of the ring defaults to twice the number of particles
         self.p = p
         self.e = epsilon
         npr.seed()
         self.sigma = self.initSigma()
+        # The configuration of the tasep is represented by a vector of positions
+        # sigma[i] = where is located the ith particle
         self.current = []
 
     def __repr__(self):
@@ -27,40 +31,55 @@ class Tasep(object):
 
     def initSigma(self):
         """
-        Lorem ipsum
+        Return a sorted vector of unique positions between 0 and n-1 indicating a site occupied by a particle
         """
         return np.sort(npr.choice(self.n, size=self.b, replace=False), kind='mergesort')
 
-    def updateSigma(self):
+    def update(self):
         """
-        Lorem ipsum
+        Update the configuration sigma applying one step of the TASEP dynamics
         """
         freetomove = np.nonzero( np.roll(self.sigma, -1) - (self.sigma + 1)%self.n )[0]
-        self.current.append(len(freetomove)/float(self.n))
+        # Particle at site i is not free to move if there is another particle at site i+1 (mod n),
+        # i.e. if sigma[i+1] == sigma[i]+1
+        self.current.append(len(freetomove)/float(self.n)) 
+        # Append the latest value of the current
         updates = npr.choice(np.arange(2), size=len(freetomove), replace=True, p=np.array([1.-self.p, self.p]))
+        # We choose which particles to move one step ahead among those free-to-move independently with probability p
         if self.sigma[freetomove[-1]] == self.n-1 and updates[-1] == 1:
+        	# if there's a particle at site n-1 about to move, then we move it with probability 1-epsilon
+        	# all in all, that particle is moved with probability p*(1-epsilon) (blockage)
             blockupdate = 0 if npr.random() < self.e else 1
             updates[-1] = blockupdate
         self.sigma[freetomove] += updates
+        # the particles selected to be moved go to the next position
         self.sigma[-1] = self.sigma[-1] % self.n
+        # the last particle may go to site n, so we apply the periodic boundary conditions
         if not self.sigma[-1]:
             self.sigma = np.roll(self.sigma, 1)
 
-    def denseSigma(self):
-        """
-        Export the configuration as a numpy vector of size self.n, tau
-        tau[i] == 1 iff there is a particle in the ith position
-        """
+    def _fullsigma(self):
         tau = np.zeros(self.n)
         tau[self.sigma] = 1
         return tau
 
+    @property
+    def fullsigma(self):
+    	"""
+        Return the configuration as a numpy vector of size self.n, tau,
+        where tau[i] == 1 iff there is a particle in the ith position
+        """
+        return self._fullsigma()
+
     def density(self, graining):
         """
-        graining is a 1D vector that indicates where to split the ring
+        Return the particles density using a coarse-graining of the ring
+        INPUT: graining is a 1D vector that indicates where to split the ring
         """
-        sections = np.split(self.denseSigma(), graining)
-        return map(densityReduction, sections)
+        sections = np.split(self.fullsigma, graining)
+        # the ring is split into subarrays according to graining,
+        # then np.mean is applied to each subarray
+        return map(np.mean, sections)
 
 
 
