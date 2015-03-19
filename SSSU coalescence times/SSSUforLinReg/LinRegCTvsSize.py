85d799c633d50127880af28e504da7ff28e01f9b
huskebasi <huskebasi@gmail.com>
2015-03-19 12:47:23 +0100

    More CT(N) for linear regression and Percentile plot
    Added more npys for Colascence Times for more values of N and plotted
    the percentiles.

--- SSSU coalescence times/SSSUforLinReg/LinRegCTvsSize.py
+++ SSSU coalescence times/SSSUforLinReg/LinRegCTvsSize.py
@@ -5,6 +5,7 @@ from scipy import stats
 
 p = 0.5
 e = 0.1
+qth = [25,50,75,90]
 
 nomefile = './N*' + '_B*' + '_p' + str(p) + '_e' + str(e) +'.npy'
 nomefile = glob(nomefile) 
@@ -12,15 +13,25 @@ nomefile = glob(nomefile)
 data = []
 N = []
 medie = []
+mediane = []
 massimi = []
+perc = []
 
-nomefile = np.sort(nomefile)
-
+nomefile.sort(key=lambda x:int(x.split('_')[1][1:])) 
+'''
+questo sort e' la ostia, Carlo tu avevi dimenticato l'int() e non
+funzionava!
+'''
 for f in nomefile:
-    N.append(2*int(f.split('_')[1][1:4]))
-    data.append(np.load(f))
-    medie.append(np.mean(data[-1]))
-    massimi.append(max(data[-1]))
+	N.append(2*int(f.split('_')[1][1:]))
+	data.append(np.load(f))
+	medie.append(np.mean(data[-1]))
+	massimi.append(max(data[-1]))
+	mediane.append(np.median(data[-1]))
+	perc.append(np.percentile(data[-1], qth))
+
+perc = np.array(perc)
+perc= perc.T
 
 xi = np.zeros(len(N))
 for i in range(len(N)):
@@ -28,27 +39,43 @@ for i in range(len(N)):
 
 Eslope, Eintercept, Er_value, Ep_value, Estd_err = stats.linregress(xi, medie)
 Mslope, Mintercept, Mr_value, Mp_value, Mstd_err = stats.linregress(xi, massimi)
+MEDslope, MEDintercept, MEDr_value, MEDp_value, Mstd_err = stats.linregress(xi, mediane)
 
-fig, (ax, bx) = plt.subplots(ncols=2)
+fig, (ax, bx, cx) = plt.subplots(ncols=3)
 
-fig.suptitle('Parallel TASEP with blockage Same Site vs Same Particle Coalescence Times', fontsize=18)
+fig.suptitle('Coalescence Times for Parallel TASEP p0.5 e.1', fontsize=18)
 
 Eline = Eslope*xi + Eintercept
+MEDline = MEDslope*xi + MEDintercept
 Mline = Mslope*xi + Mintercept
 
 ax.plot(N,Eline,'r-',N,medie,'o')
-
-
 ax.set_ylabel('Mean of Coalescence Times')
 ax.set_xlabel('Number of Sites of the Ring')
-#plt.title('Parallel TASEP p0.5 e0.1 line regression of the Mean Coalescence Times')
-ax.text(60, 112, 'Slope = %f \nIntercept = %f' %(Eslope, Eintercept), fontsize=16)
+ax.text(60, 300, 'Slope = %f \nIntercept = %f' %(Eslope, Eintercept), fontsize=16)
 
-
-
-bx.plot(N,Mline,'r-',N,massimi,'g^')
-bx.text(65, 60000, 'Slope = %f \nIntercept = %f' %(Mslope, Mintercept), fontsize=16)
-bx.set_ylabel('Max of Coalescence Times')
+bx.plot(N,MEDline,'r-',N,mediane,'x')
+bx.set_ylabel('Median of Coalescence Times')
 bx.set_xlabel('Number of Sites of the Ring')
-#plt.title('Parallel TASEP e0.1 p0.5 line regression of the Max Coalescence Times')
+bx.text(60, 300, 'Slope = %f \nIntercept = %f' %(MEDslope, MEDintercept), fontsize=16)
+
+cx.plot(N,Mline,'r-',N,massimi,'g^')
+cx.text(65, 600000, 'Slope = %f \nIntercept = %f' %(Mslope, Mintercept), fontsize=16)
+cx.set_ylabel('Max of Coalescence Times')
+cx.set_xlabel('Number of Sites of the Ring')
+
 plt.show()
+
+fig = plt.figure()
+
+for row, lab in zip(perc[::-1],qth[::-1]): 
+	plt.plot(N,row, label=lab)
+'''
+ho usato la extended slice syntax solo per avere la legenda in ordine decrescente
+'''
+plt.legend(loc=2, title= 'Percentiles')
+plt.ylabel('Values of Percentiles of Coalescence Times')
+plt.xlabel('Number of Sites of the Ring')
+plt.title('Percentiles of Coealescence Times of Parallel TASEP p0.5 e0.1')
+
+plt.show(fig)
